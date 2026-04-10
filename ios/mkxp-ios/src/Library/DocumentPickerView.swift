30b98eb31ed1eb2ba91263622d5db00eaf126f0e
Mateo M. <hello@mateo.sh>
2026-04-10 18:27:18 +0200

    feat: add game card, import validation, zip extraction, and cancellable imports

---
+++ ios/mkxp-ios/src/Library/DocumentPickerView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct DocumentPickerView: UIViewControllerRepresentable {
+    var onPick: (URL) -> Void
+
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: [.folder, .zip])
+        picker.allowsMultipleSelection = false
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator { Coordinator(onPick: onPick) }
+
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        let onPick: (URL) -> Void
+        init(onPick: @escaping (URL) -> Void) { self.onPick = onPick }
+
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            onPick(url)
+        }
+    }
+}
